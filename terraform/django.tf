f12e0e5e2c2415b95862aa692d594a026bb8ef47
jwygoda <wygoda.jaroslaw@gmail.com>
2020-07-05 13:11:22 +0200

    Migrate to api gateway v2

--- terraform/django.tf
+++ terraform/django.tf
@@ -1,21 +1,10 @@
 module "django" {
   source                 = "./modules/django"
   lambda_function_name   = "djambda"
-  lambda_handler         = "djambda.awsgi.lambda_handler"
-  stage                  = "dev"
-  aws_region             = var.aws_region
-  create_lambda_function = true
-  default_from_email     = var.default_from_email
-  enable_api_gateway     = true
-}
-
-module "djangov2" {
-  source                 = "./modules/django"
-  lambda_function_name   = "djambda"
   lambda_handler         = "djambda.lgi.application"
   stage                  = "dev"
   aws_region             = var.aws_region
-  create_lambda_function = false
+  create_lambda_function = true
   default_from_email     = var.default_from_email
   enable_api_gatewayv2   = true
 }
