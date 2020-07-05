f896de40e50316636c133f14f8d3595e1539da26
jwygoda <wygoda.jaroslaw@gmail.com>
2020-07-05 19:42:40 +0200

    Fix event processing

--- src/lgi.py
+++ src/lgi.py
@@ -16,32 +16,27 @@ _default_route_key = "$default"
 
 
 class LGIRequest(HttpRequest):
-    def __init__(self, payload):
+    def __init__(self, event):
+        self.event = event
         self._read_started = False
         self.resolver_match = None
-        script_name = (
-            "" if payload["routeKey"] == _default_route_key else payload["routeKey"]
-        )
-        self.path_info = payload["requestContext"]["http"]["path"]
-        self.path = "%s/%s" % (
-            script_name.rstrip("/"),
-            self.path_info.replace("/", "", 1),
-        )
-        self.method = payload["requestContext"]["http"]["method"]
+        self.path_info = event["requestContext"]["http"]["path"]
+        self.path = self.path_info
+        self.method = event["requestContext"]["http"]["method"]
 
         self.META = {
             "REQUEST_METHOD": self.method,
-            "QUERY_STRING": payload["rawQueryString"],
-            "SCRIPT_NAME": script_name,
+            "QUERY_STRING": event["rawQueryString"],
+            "SCRIPT_NAME": "",
             "PATH_INFO": self.path_info,
-            "REMOTE_ADDR": payload["requestContext"]["http"]["sourceIp"],
-            "REMOTE_HOST": payload["requestContext"]["http"]["sourceIp"],
-            "SERVER_NAME": payload["requestContext"]["domainName"],
+            "REMOTE_ADDR": event["requestContext"]["http"]["sourceIp"],
+            "REMOTE_HOST": event["requestContext"]["http"]["sourceIp"],
+            "SERVER_NAME": event["requestContext"]["domainName"],
             "SERVER_PORT": "443",
             "wsgi.multithread": False,
             "wsgi.multiprocess": False,
         }
-        for name, value in payload["headers"].items():
+        for name, value in event["headers"].items():
             corrected_name = name.replace("-", "_").upper()
             if corrected_name not in ("CONTENT_TYPE", "CONTENT_LENGTH"):
                 corrected_name = f"HTTP_{corrected_name}"
@@ -62,7 +57,7 @@ class LGIRequest(HttpRequest):
             self._load_post_and_files()
         return self._post
 
-    def _set_post(self):
+    def _set_post(self, post):
         self._post = post
 
     POST = property(_get_post, _set_post)
@@ -75,7 +70,8 @@ class LGIRequest(HttpRequest):
 
     @cached_property
     def COOKIES(self):
-        return parse_cookie(self.META.get("HTTP_COOKIE", ""))
+        # parse_cookie expects cookies list as a ; separated string
+        return parse_cookie(";".join(self.event["cookies"]))
 
 
 class LGIHandler(base.BaseHandler):
@@ -85,40 +81,32 @@ class LGIHandler(base.BaseHandler):
         super().__init__()
         self.load_middleware()
 
-    def __call__(self, payload, context):
-        print(payload, context)
+    def __call__(self, event, context):
         # handle manage.py
-        if "manage" in payload:
+        if "manage" in event:
             output = StringIO()
-            management.call_command(*payload["manage"], stdout=output)
+            management.call_command(*event["manage"], stdout=output)
             return {"output": output.getvalue()}
 
         # handle api gateway
-        version = payload["version"]
+        version = event["version"]
         if version != "2.0":
             raise ValueError(f"{version} format version is not supported")
 
-        set_script_prefix(self.get_script_prefix(payload))
-        signals.request_started.send(sender=self.__class__, payload=payload)
-        request = self.request_class(payload)
+        set_script_prefix(getattr(settings, "FORCE_SCRIPT_NAME", ""))
+        signals.request_started.send(sender=self.__class__, event=event)
+        request = self.request_class(event)
         response = self.get_response(request)
 
         response._handler_class = self.__class__
 
-        return json.dumps(
-            {
-                "body": response.content.decode(),
-                "headers": list(response.items()),
-                "statusCode": response.status_code,
-                "isBase64Encoded": False,
-                "cookies": list(response.cookies.values()),
-            }
-        )
-
-    def get_script_prefix(self, payload):
-        if settings.FORCE_SCRIPT_NAME:
-            return settings.FORCE_SCRIPT_NAME
-        return "" if payload["routeKey"] == _default_route_key else payload["routeKey"]
+        return {
+            "body": response.content.decode(),
+            "headers": list(response.items()),
+            "statusCode": response.status_code,
+            "isBase64Encoded": False,
+            "cookies": list(response.cookies.values()),
+        }
 
 
 def get_lgi_application():
