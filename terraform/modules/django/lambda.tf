7f2b71aa12fb6f238133afa50f87a2560bbe6482
trgordonb <gordoncheng@outlook.com>
2023-11-02 08:56:15 +0800

    update DATABASE_URL composition

--- terraform/modules/django/lambda.tf
+++ terraform/modules/django/lambda.tf
@@ -116,7 +116,7 @@ resource "aws_lambda_function" "function" {
       {
         ALLOWED_HOSTS = "*"
         DEBUG = "False"
-        DATABASE_URL = "postgres://${module.db.this_db_instance_username}:${module.db.this_db_instance_password}@${module.db.this_db_instance_address}:${module.db.this_db_instance_port}/${var.lambda_function_name}_${keys(local.dist_manifest)[count.index]}"
+        DATABASE_URL = "postgres://${module.db.db_instance_username}:${module.db.db_instance_password}@${module.db.db_instance_address}:${module.db.db_instance_port}/${var.lambda_function_name}_${keys(local.dist_manifest)[count.index]}"
         FORCE_SCRIPT_NAME = "/${keys(local.dist_manifest)[count.index]}/"
         DJANGO_SUPERUSER_PASSWORD=random_password.password.result
         ENABLE_MANIFEST_STORAGE = "True"
