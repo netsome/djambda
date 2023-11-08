dc6b44aa329931c0910e3bc00ffa879696b62716
trgordonb <gordoncheng@outlook.com>
2023-11-08 09:50:01 +0800

    add new db subnet setting

--- terraform/modules/django/rds.tf
+++ terraform/modules/django/rds.tf
@@ -64,7 +64,8 @@ module "db" {
 
   create_db_option_group    = false
   create_db_parameter_group = false
-
+  create_db_subnet_group    = true
+  db_subnet_group_name      = module.vpc.database_subnet_group_name
 }
 
 ############
