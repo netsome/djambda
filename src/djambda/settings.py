fc9322ef399054eb43063d8d7f8cbfb8b4d41ced
trgordonb <gordoncheng@outlook.com>
2023-11-09 21:27:33 +0800

    add template context processor

--- src/djambda/settings.py
+++ src/djambda/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                'django.template.context_processors.static',
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
